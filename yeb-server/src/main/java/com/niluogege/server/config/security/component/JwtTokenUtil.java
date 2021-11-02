220a92dd6f364f9f9e9c42b2191eeadf32de7838
luoChen <hank.luo@xianghuanji.com>
2021-11-02 18:17:27 +0800

    添加 jjwt

---
+++ yeb-server/src/main/java/com/niluogege/server/config/security/component/JwtTokenUtil.java
@@ -0,0 +1,128 @@
+package com.niluogege.server.config.security.component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtTokenUtil {
+
+    //也可以自定义字段，不一定使用 sub
+    private static final String CLAIM_KEY_USERNAME = "sub";
+    private static final String CLAIM_KEY_CREATED = "created";
+
+
+    @Value("${jwt.expiration}")
+    private Long expiration;//token 到期时间
+
+    @Value("${jwt.secret}")
+    private String secret;//token 签名秘钥
+
+
+    /**
+     * 根据用户信息生成token
+     */
+    public String generateToken(UserDetails userDetails) {
+        HashMap<String, Object> claims = new HashMap<>();
+        claims.put(CLAIM_KEY_USERNAME, userDetails.getUsername());
+        claims.put(CLAIM_KEY_CREATED, new Date());
+        return generateToken(claims);
+    }
+
+    /**
+     * 通过载荷（参数）生成token
+     *
+     * @param claims
+     * @return
+     */
+    public String generateToken(Map<String, Object> claims) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(generateExpirationDate())
+                .signWith(SignatureAlgorithm.ES512, secret)
+                .compact();
+    }
+
+    /**
+     * token 到期时间
+     */
+    private Date generateExpirationDate() {
+        return new Date(System.currentTimeMillis() + expiration * 1000);
+    }
+
+
+    /**
+     * 验证token是否有效
+     *
+     * @param token
+     * @param userDetails
+     * @return
+     */
+    public boolean validateToken(String token, UserDetails userDetails) {
+        String userName = getUserNameFormToken(token);
+        return userName.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+
+    public String getUserNameFormToken(String token) {
+        Claims claims = getClaimsFormToken(token);
+        //存的时候是吧 username 存到了 sub 中，所以可以调用 getSubject 来取
+        String userName = claims.getSubject();
+        return userName;
+    }
+
+    public Claims getClaimsFormToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    /**
+     * token 是否过期
+     *
+     * @param token
+     * @return
+     */
+    private boolean isTokenExpired(String token) {
+        Date expiredDate = getExpiredDateFromToken(token);
+        return expiredDate.before(new Date());
+    }
+
+
+    /**
+     * 获取过期时间
+     *
+     * @param token
+     * @return
+     */
+    private Date getExpiredDateFromToken(String token) {
+        Claims claims = getClaimsFormToken(token);
+        return claims.getExpiration();
+    }
+
+    /**
+     * 是否可以刷新，如果没过期则不能刷新
+     * @param token
+     * @return
+     */
+    public boolean canRefresh(String token){
+        return isTokenExpired(token);
+    }
+
+    /**
+     * 刷新 token
+     * @param token
+     * @return
+     */
+    public String refreshToken(String token){
+        Claims claims = getClaimsFormToken(token);
+        claims.put(CLAIM_KEY_CREATED,new Date());
+        return  generateToken(claims);
+    }
+}
