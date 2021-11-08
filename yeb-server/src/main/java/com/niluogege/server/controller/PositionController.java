febc4a8169eb27bcdc8e0088532aa49ab9abc5cf
luoChen <hank.luo@xianghuanji.com>
2021-11-08 14:21:52 +0800

    职位 增删改查

--- yeb-server/src/main/java/com/niluogege/server/controller/PositionController.java
+++ yeb-server/src/main/java/com/niluogege/server/controller/PositionController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 /**
  * <p>
@@ -26,14 +27,17 @@ public class PositionController {
     private IPositionService positionService;
 
     @ApiOperation("获取所有职位")
-    @GetMapping("/getAllPositions")
+    @GetMapping("/")
     public RespBean getAllPositions() {
         return RespBean.success(positionService.list());
     }
 
     @ApiOperation("添加职位")
-    @PostMapping("/addPosition")
+    @PostMapping("/")
     public RespBean addPosition(@RequestBody Position position){
+        if (position.getName().isEmpty()){
+            return RespBean.error("名称非法");
+        }
         position.setCreateDate(LocalDateTime.now());
         boolean saved = positionService.save(position);
         if (saved){
@@ -41,6 +45,38 @@ public class PositionController {
         }
 
         return RespBean.error("添加失败");
+    }
+
+    @ApiOperation("更新职位")
+    @PutMapping("/")
+    public RespBean updatePosition(@RequestBody Position position){
+       if (positionService.updateById(position)){
+           return RespBean.success("更新成功");
+       }
+
+       return RespBean.error("更新失败");
+    }
+
+
+    @ApiOperation("删除职位信息")
+    @DeleteMapping("/{id}")
+    public RespBean deletePosition(@PathVariable Integer id){
+        if (positionService.removeById(id)){
+            return RespBean.success("删除成功");
+        }
+
+        return RespBean.error("删除失败");
+    }
+
+
+    @ApiOperation("批量删除职位信息")
+    @DeleteMapping("/")
+    public RespBean deletePositionByIds(Integer[] ids){
+        if (positionService.removeByIds(Arrays.asList(ids))){
+            return RespBean.success("删除成功");
+        }
+
+        return RespBean.error("删除失败");
     }
 
 }
