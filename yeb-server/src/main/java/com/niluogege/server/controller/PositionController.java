a94e79fb5f04c9663c3d0916ceb5351a96da9b88
luoChen <hank.luo@xianghuanji.com>
2021-11-08 13:50:16 +0800

    通过 JsonFormat 注解 转换 数据库中取出来的时间

--- yeb-server/src/main/java/com/niluogege/server/controller/PositionController.java
+++ yeb-server/src/main/java/com/niluogege/server/controller/PositionController.java
@@ -1,19 +1,33 @@
 package com.niluogege.server.controller;
 
 
+import com.niluogege.server.pojo.RespBean;
+import com.niluogege.server.service.IPositionService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author niluogege
  * @since 2021-10-29
  */
 @RestController
-@RequestMapping("/position")
+@RequestMapping("/system/basic/pos")
 public class PositionController {
+
+    @Autowired
+    private IPositionService positionService;
+
+    @ApiOperation("获取所有职位")
+    @GetMapping("/getAllPositions")
+    public RespBean getAllPositions() {
+        return RespBean.success(positionService.list());
+    }
 
 }
